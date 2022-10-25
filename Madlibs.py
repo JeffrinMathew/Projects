03ec869ff9eb6f74a1ca12be70c5210ebc829e1a
Jeffrin Mathew Kurian <116439517+JeffrinMathew@users.noreply.github.com>
2022-10-25 17:09:16 +0400

    Add files via upload

---
+++ Madlibs.py
@@ -0,0 +1,26 @@
+adj=input("Adjective: ")
+verb1=input("Verb: ")
+verb2=input("Verb: ")
+famous_person=input("Famous person: ")
+animal_name=input("Animal name: ")
+verb3=input("Verb: ")
+noun1=input("Noun: ")
+noun2=input("Noun: ")
+noun3=input("Noun: ") 
+noun4=input("Noun: ")
+verb4=input("Verb: ")
+verb5=input("Verb: ")
+verb6=input("Verb: ")
+madlib = f"Computer Programming is so {adj}! It makes me so exited all the time because \
+        i love {verb1}. Stay hydrated and {verb2} like you are {famous_person}!,\
+        Now comes the funny part of the Game!\
+        There wasn't a {animal_name} in the sky, but that was not what caught her attention.\
+         It was the {noun1} . The deep green {noun2} that isn't the color of {noun3},\
+        but came with these. She knew what was coming and she hoped she was prepared.\
+        She heard the loud {verb3} before she ever saw the result. \
+        It had been so loud that it had actually made her jump back in his seat.\
+        As soon as she recovered from the surprise, she saw the [{noun4}] in the windshield.\
+        It seemed to be {verb4} towards her! {verb5}."\
+
+print(madlib)
+
