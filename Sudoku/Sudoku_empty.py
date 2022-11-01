c9183090bed76dfa231aed834c53326a16d258f2
Jeffrin Mathew Kurian <116439517+JeffrinMathew@users.noreply.github.com>
2022-11-01 15:41:44 +0400

    Add files via upload

---
+++ Sudoku/Sudoku_empty.py
@@ -0,0 +1,13 @@
+def find_next_empty(puzzle):
+    # finds the next row, col on puzzle that's not filled yet -->  represent these with -1
+    # returns a row, col tuple (or (None, None) if there is none)
+    pass
+
+def is_valid(puzzle, guess, row, col):
+    # figures out whether the guess at the row/col of the puzzle is a valid guess
+    # returns True or False
+    pass
+
+def solve_sudoku(puzzle):
+   
+    pass
